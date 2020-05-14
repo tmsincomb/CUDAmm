16eaa2557f13ee0640606236f049f6fc16aefa76
Troy Sincomb <troysincomb@gmail.com>
2020-05-13 21:52:28 -0700

    -

--- project.py
+++ project.py
@@ -98,11 +98,10 @@ class MatrixMultiplication:
         print(dim_grid_x)
         print(dim_grid_y)
 
-        # set grid size
-        # if (width % self.dim_block != 0) and (height % self.dim_block != 0):
-        #     grid=(dim_grid_x+1, dim_grid_y+1, 1)
-        # else:
-        grid=(dim_grid_x, dim_grid_y, 1)
+        if (width % self.dim_block != 0) and (height % self.dim_block != 0):
+            grid=(dim_grid_x+1, dim_grid_y+1, 1)
+        else:
+            grid=(dim_grid_x, dim_grid_y, 1)
 
         # Call specific function from CUDA kernel
         dot_product = self.module.get_function("dot");
